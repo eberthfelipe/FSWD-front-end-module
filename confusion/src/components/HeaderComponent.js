0129169bb46f4c2c3c2d39631ea1236b94a530fe
Eberth Felipe Castro da Cruz <eberth.c@samsung.com>
2019-12-18 13:37:30 -0400

    Uncontrolled forms part 1: add login modal

--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron} from 'reactstrap';
+import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 class Header extends Component{
@@ -8,10 +9,12 @@ class Header extends Component{
         super(props);
     
         this.state = {
-          isNavOpen: false
+          isNavOpen: false,
+          isModalOpen: false
         };
+
         this.toogleNav = this.toogleNav.bind(this);
-    
+        this.toogleModal = this.toogleModal.bind(this);
       }
 
     render(){
@@ -48,6 +51,13 @@ class Header extends Component{
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toogleModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span>Login
+                                    </Button>
+                                </NavItem>
+                            </Nav> 
                         </Collapse>
                     </div>  
                 </Navbar>
@@ -61,6 +71,12 @@ class Header extends Component{
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toogleModal}>
+                    <ModalHeader toggle={this.toogleModal}>Login</ModalHeader>
+                    <ModalBody>
+
+                    </ModalBody>
+                </Modal>
             </>
         );
     }
@@ -68,6 +84,12 @@ class Header extends Component{
     toogleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    toogleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
         });
     }
 }
