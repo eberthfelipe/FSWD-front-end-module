6efb8529ae51a80fedb7bb4e3509f7172c1cda7d
EberthFelipe <eberthfelipe@gmail.com>
2019-12-18 21:36:03 -0400

    Uncontrolled Form complete

--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
-    Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Label, Input
+} from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 class Header extends Component{
@@ -15,6 +17,7 @@ class Header extends Component{
 
         this.toogleNav = this.toogleNav.bind(this);
         this.toogleModal = this.toogleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
       }
 
     render(){
@@ -74,7 +77,26 @@ class Header extends Component{
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toogleModal}>
                     <ModalHeader toggle={this.toogleModal}>Login</ModalHeader>
                     <ModalBody>
-
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                        innerRef={(input) => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
                     </ModalBody>
                 </Modal>
             </>
@@ -91,6 +113,13 @@ class Header extends Component{
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
+    }
+
+    handleLogin(event){
+        this.toogleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
     }
 }
 
